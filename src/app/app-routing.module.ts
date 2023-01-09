badfb4d10f48ffb976a2dd2ca1737ce306d58655
Don-lechuga <christian.sabrerar@gmail.com>
2023-01-09 11:23:35 -0500

    cambio en ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 
 const routes: Routes = [
-  {path: "", component: IndexComponent}
+  {path: "/", component: IndexComponent}
 ];
 
 @NgModule({
